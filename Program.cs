d9d283d65c09d3f781f5cff247f27714b15aff96
Majad In <majadin@Majads-MacBook-Pro.local>
2020-12-01 20:50:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.Title = "DevThoughts"; 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hello My name is Tom 😀");
+            Console.ReadLine();
+
+            Console.WriteLine("Tom! Bobby's old fried from Stavanger 😎");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("No actully I am a dragen!!! 🐲");
+            Console.ReadKey();
+        }
+    }
+}
